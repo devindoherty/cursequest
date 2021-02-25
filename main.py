e5f41a0e355e773f4cc641dd8b2068f8be4f7164
Devin Doherty <devincdoherty@gmail.com>
2021-02-24 23:32:04 -0500

    Add atlas.py
    - Removed map.py due to map function in standard library
    - Added atlas.py
    - Working on a dictionary for a rough version of the world map

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import curses
 import time
+import atlas 
+
 
 MENU_OPTIONS = {
         0: "travel",
@@ -14,11 +16,11 @@ MENU_OPTIONS = {
 ASCII_ENTER = 10
 
 
-
 # Setting up two windows, one for ASCII art and maps and whatnot, the other for text output/input
-def panes(stdscr):
-    height, width = stdscr.getmaxyx()
-   
+def screens(stdscr):
+	height, width = stdscr.getmaxyx()
+	curses.start_color()
+
     # Finding starting y,x for art and text panes
     art_x = 0
     art_y = 0
@@ -27,18 +29,25 @@ def panes(stdscr):
 
     # Setup the Art Pane with artscr
     artscr = curses.newwin(int(height // 2) - 1, width, art_y, art_x)
-    artscr.addstr(1, 1, "This is the Art Window")
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    artscr.addstr(1, 1, "This is the Art Window", curses.color_pair(2))
     artscr.box()
     artscr.refresh()
 
     # Setup the Text Pane with textscr
     textscr = curses.newwin(int((height // 2) - 1) , width, text_y, text_x)
-    textscr.addstr(1, 1, "This is the Text Window")  
+    textscr.addstr(1, 1, "This is the Text Window", curses.color_pair(2))
     textscr.keypad(True)
     textscr.box()
     textscr.refresh()
 
-    return textscr, artscr
+#    # Setup the test window
+#    testscr = curses.newwin(10, 10, 10, 10)
+#    testscr.addstr(1, 1, "Mobile Test Window! 1, 2, 3, A, B, C")
+#    testscr.box()
+#    testscr.refresh()
+    
+    return textscr, artscr #testscr
     
 # Determining menu placement, formatting menu
 def menu(textscr, current_row):
@@ -64,46 +73,33 @@ def menu(textscr, current_row):
     textscr.refresh()
 
 
-#def ascii_scan():
-#    f = open("art.txt", "r")
-#    print(f.read())
-#
-#    # Reading external files STILL BROKEN ARGHHHH
-#
-#    with open("art.txt", "r", encoding ="utf8") as f:
-#        lines = f.readlines()
-#
-#    for a in lines:
-#        artscr.addstr(20, 5, a.rstrip())
-#        artscr.refresh()
-#
-#
-#    # Color Testing
-#    curses.start_color()
-#    curses.use_default_colors()
-#    for i in range(0, curses.COLORS):
-#        curses.init_pair(i + 1, i, -1)
-#    try:
-#        for i in range(0, 255):
-#           textscr.addstr(str(i), curses.color_pair(i))
-#    except curses.ERR:
-#        pass
-#
-#
-#    # Print our different windows
-#
-#
-#   
-#    # Setup the test window
-#    testscr = curses.newwin(10, 10, 10, 10)
-#    
-#    # Draw a box around the test window and print a string
-#    
-#    testscr.addstr(1, 1, "Mobile Test Window! 1, 2, 3, A, B, C")
-#    testscr.box()
-#    testscr.refresh()
-   
+def scan():
+    f = open("art.txt", "r")
+    print(f.read())
 
+    # Reading external files STILL BROKEN ARGHHHH
+
+    with open("art.txt", "r", encoding ="utf8") as f:
+        lines = f.readlines()
+
+    for a in lines:
+        artscr.addstr(20, 5, a.rstrip())
+        artscr.refresh()
+
+
+# Color Testing
+def color_test(textscr):
+    curses.start_color()
+    curses.use_default_colors()
+    for i in range(0, curses.COLORS):
+        curses.init_pair(i + 1, i, -1)
+    try:
+        for i in range(0, 255):
+           textscr.addstr(str(i), curses.color_pair(i))
+    except curses.ERR:
+        pass
+
+   
 def curses_main(stdscr):
     # Blank the canvas
     stdscr = curses.initscr()
@@ -116,16 +112,15 @@ def curses_main(stdscr):
 
     # Finding Terminal size. Height = y, width = x
     height, width = stdscr.getmaxyx()
+   
+    textscr, artscr = screens(stdscr)
+    
+    color_test(textscr)
 
-     
-    textscr, artscr = panes(stdscr)
-    
-    
     # Setup the menu and call menu function
 
     current_row = 0   
-       
-    
+        
     menu(textscr, current_row)
 
     # Menu Loop 
@@ -141,45 +136,24 @@ def curses_main(stdscr):
             current_row += 1
             artscr.addstr(18, 5, "DOWN KEY PRESSED")
                
-        elif key == ord("t"):
+        elif key == ord("t") or key == ASCII_ENTER and menu_selection == "travel":
             travel(artscr)
 
-        elif key == ord("e"):
+        elif key == ord("e") or key == ASCII_ENTER and menu_selection == "explore":
             explore(artscr)
 
-        elif key == ord("i"):
+        elif key == ord("i") or key == ASCII_ENTER and menu_selection == "interact":
             interact(textscr)
 
-        elif key == ord("c"):
+        elif key == ord("c") or key == ASCII_ENTER and menu_selection == "character":
             character(artscr)
 
-        elif key == ord("j"):
+        elif key == ord("j") or key == ASCII_ENTER and menu_selection == "journal":
             journal(artscr)
 
-        elif key == ord("q"):
+        elif key == ord("q") or key == ASCII_ENTER and menu_selection == "quit":
             break
 
-
-        if key == ASCII_ENTER and menu_selection == "travel":
-            travel(artscr)
-
-        if key == ASCII_ENTER and menu_selection == "explore":
-            explore(artscr)
-
-        if key == ASCII_ENTER and menu_selection == "interact":
-            interact(textscr)
-
-        if key == ASCII_ENTER and menu_selection == "character":
-            character(artscr)
-
-        if key == ASCII_ENTER and menu_selection == "journal":
-            journal(artscr)
-        
-        if key == ASCII_ENTER and menu_selection == "quit":
-            break
-
-
-        
         menu(textscr, current_row)
 
         textscr.refresh()
@@ -191,12 +165,13 @@ def travel(artscr):
     art_height, art_width = artscr.getmaxyx()
 
     artscr.addstr(art_height // 2, art_width // 2, "YE OLDE MAP OF KLATHIA")
-
+    atlas.draw_map(artscr, world_map) 
 
 def explore(artscr):
     artscr.addstr(4, 3, "EXPLORATION ART")
 
 def interact(textscr):
+    textscr.erase
     textscr.addstr("INTERACTION OPTIONS MENU")
 
 def character(artscr):
@@ -211,7 +186,7 @@ def character(artscr):
     artscr.addstr((art_height // 2) + 7, (art_width // 2), "|          | \n")
     artscr.addstr((art_height // 2) + 8, (art_width // 2), " \ ______ /  \n")
     artscr.addstr((art_height // 2) + 9, (art_width // 2), "-------------\n")
-    artscr.addstr((art_height // 2) + 10, (art_width // 2), "    KRYLL    \n")
+    artscr.addstr((art_height // 2) + 10, (art_width // 2),"KRYLL the KLATHIAN\n")
 
 def journal(artscr):
     artscr.addstr(5, 3, "JOURNAL ENTRY SKETCH")    
@@ -221,6 +196,7 @@ def journal(artscr):
 #class JournalScreen():
 #    __init__():
 #        quest_log = 
+
 #    # Color Testing
 #    curses.start_color()
 #    curses.use_default_colors()
