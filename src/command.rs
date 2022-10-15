dd107af8685c15a616a4a5cd641b597b84d8664d
Devin Doherty <devin@devindoherty.com>
2022-10-14 23:10:14 -0400

    additional refactor of input & command

--- src/command.rs
+++ src/command.rs
@@ -11,14 +11,25 @@ pub trait Command {
 
 impl Command for VirtualKeyCode {
     fn execute(&self, gs: &mut State) {
-        if gs.run_mode == RunMode::Start { 
-            match self {
-                Self::Up => gs.commands = gs.startmenu.manage(VirtualKeyCode::Up),
-                Self::Down => gs.commands = gs.startmenu.manage(VirtualKeyCode::Down),
-                Self::Right => gs.commands = gs.startmenu.manage(VirtualKeyCode::Left),
-                Self::Right => gs.commands = gs.startmenu.manage(VirtualKeyCode::Right),
-                _ => todo!(),
+        match self {
+            Self::Up => if gs.run_mode == RunMode::Start {
+                gs.commands = gs.startmenu.manage(VirtualKeyCode::Up);
+            } 
+            else if gs.run_mode == RunMode::Intro {
+
             }
+            else if gs.run_mode == RunMode::Running {
+                gs.commands = gs.player.map_move(0, 1);
+            },
+            
+            Self::Down => if gs.run_mode == RunMode::Start {
+                gs.commands = gs.startmenu.manage(VirtualKeyCode::Down);
+            }
+            
+            Self::Right => gs.commands = gs.startmenu.manage(VirtualKeyCode::Left),
+            Self::Right => gs.commands = gs.startmenu.manage(VirtualKeyCode::Right),
+            _ => todo!(),
         }
     }
 }
+
