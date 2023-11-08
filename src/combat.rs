5f84c95fb62cca347d667e34b29db9e928570877
Devin Doherty <devin@devindoherty.com>
2023-11-08 10:26:22 -0500

    Initial combat structs

---
+++ src/combat.rs
@@ -0,0 +1,14 @@
+use bracket_lib as bracket;
+use bracket::prelude::*;
+use bracket::random::*;
+
+
+struct CombatCoordinator {
+    turn_counter: u32,
+}
+
+
+struct Action {
+    name: String,
+    cost: 
+}
